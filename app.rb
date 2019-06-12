cf56a03c20c7197eeb76e99043c2420c3c786720
Alex Merecka <merecka@gmail.com>
2019-06-12 02:36:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/hello' do
+		status 200
+		erb :hello
 	end
 
+	get '/goodbye' do
+		status 200
+		erb :goodbye
+	end
+
+	get '/date' do
+		status 200
+		erb :date
+	end
 
 end
